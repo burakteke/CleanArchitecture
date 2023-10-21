279b7cf59b75aec048206dd2c50f8b21d94c7981
Burak Teke <burakteke37@gmail.com>
2023-10-22 02:14:05 +0300

    API & db connection

--- Basket.API/Controllers/BasketController.cs
+++ Basket.API/Controllers/BasketController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Basket.Application.Interfaces;
+using Basket.Core.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Basket.API.Controllers
@@ -7,5 +9,47 @@ namespace Basket.API.Controllers
     [ApiController]
     public class BasketController : ControllerBase
     {
+        private readonly IBasketService _basketService;
+
+        public BasketController(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+
+        [HttpGet("id")]
+        public ActionResult<BasketModel> Get(int id)
+        {
+            var basket = _basketService.Get(id);
+            if(basket == null)
+            {
+                return NotFound();
+            }
+            return basket;
+        }
+
+        [HttpPost]
+        public ActionResult<BasketModel> Create(BasketModel model)
+        {
+            var createdSepet = _basketService.Create(model);
+            return CreatedAtAction(nameof(Get), new { id = createdSepet.Id }, createdSepet);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, BasketModel model)
+        {
+            if (id != model.Id)
+            {
+                return BadRequest();
+            }
+            _basketService.Update(model);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _basketService.Delete(id);
+            return NoContent();
+        }
     }
 }
